13ae34f6e12ec8c53391437b8a079e55f1c12b2a
Olga Lobanova <arapova@list.ru>
2022-09-29 15:33:33 +0300

    Задача 29

---
+++ Ex29/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+// Ограничения:
+// - Должна быть отдельная функция, которая создаёт массив и отдельная функция вывода
+// - Внутри функций (кроме функции вывода) НЕЛЬЗЯ использовать класс Console. Только в основном блоке кода или в функции вывода
+void printArray(int[] array)
+{
+    Console.Write("[ ");
+    for (int i = 0; i < 8; i++)
+        if (i < 8 - 1)
+            Console.Write(array[i] + ", ");
+        else
+            Console.Write(array[i]);
+    Console.WriteLine(" ]");
+}
+int[] fillArray()
+{
+    Random rnd = new Random();
+    int[] array = new int[rnd.Next(10, 100)];
+    int len = 8;
+    for (int i = 0; i < len; i++)
+    {
+        array[i] = rnd.Next(1, 10);
+    }
+    return array;
+}
+printArray(fillArray());
